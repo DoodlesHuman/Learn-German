cda2c1bd2492bc84588d4f2c23a632afbf01912c
DoodlesHuman <amosrr@gmail.com>
2025-07-04 14:21:51 +0200

    subprocess force installation added for sentencepiece

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import subprocess
+import sys
+
+subprocess.check_call([sys.executable, "-m", "pip", "install", "sentencepiece"])
+
+
 import streamlit as st
 from transformers import pipeline
 import language_tool_python
